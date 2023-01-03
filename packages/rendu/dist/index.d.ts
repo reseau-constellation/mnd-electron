17d38bc666e97d6f744de67b9b9104ee3f5cbb0f
julienmalard <julien.malard@mail.mcgill.ca>
2023-01-02 22:01:59 -0500

    Ignorer dist

--- packages/rendu/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { MandataireClientÉlectronPrincipal, générerMandataireÉlectronPrincipal, } from "./mandataire.js";
-export { GestionnaireServeur, } from "./serveurWS.js";
--- packages/rendu/dist/index.js
+++
@@ -1,3 +0,0 @@
-export { MandataireClientÉlectronPrincipal, générerMandataireÉlectronPrincipal, } from "./mandataire.js";
-export { GestionnaireServeur, } from "./serveurWS.js";
-//# sourceMappingURL=index.js.map
